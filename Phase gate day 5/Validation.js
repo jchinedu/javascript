54973be672172cef7a6ecb5c67ad56cce7752a48
jchinedu <jchinedu080@gmail.com>
2025-05-30 11:56:02 +0100

    'today'

--- Phase gate day 5/Validation.js
+++ Phase gate day 5/Validation.js
@@ -1,7 +1,9 @@
 function AtmValidation(cardnumber){
- if(cardnumber != NaN){
-  console.log('sorry, not a number')
-	};
+ for (let i = 0; i < cardnumber.length; i++) {
+    if (typeof cardnumber[i] !== "number" || isNaN(cardnumber[i])) {
+      return { valid: false, reason: "Card number contains non-numeric values" };
+    }
+  }
  let visacard = {
 		valid: true,
 		issuer: "Visacard",
@@ -38,7 +40,7 @@ firstnum = [1,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
 secondnum = [2,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
 thirdnum = [3,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
 fourthnum = [4,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
-fifthnum = [5,2,3,4,5,6,7,w,9,3,5,6,7,8,6];
+fifthnum = [5,2,3,4,5,6,7,1,9,3,5,6,7,8,6];
 
 
 
