9adb116ef60cdc8680de486e26ec1c149df9e86a
jchinedu <jchinedu080@gmail.com>
2025-05-30 11:51:14 +0100

    'today'

---
+++ Phase gate day 5/Validation.js
@@ -0,0 +1,49 @@
+function AtmValidation(cardnumber){
+ if(cardnumber != NaN){
+  console.log('sorry, not a number')
+	};
+ let visacard = {
+		valid: true,
+		issuer: "Visacard",
+		};
+
+let Mastercard = {
+		valid: true,
+		issuer: "Mastercard",
+		};
+let Discover = {
+		valid: true,
+		issuer: "Discover",
+		};
+let AmericanExpress = {
+		valid: true,
+		issuer: "AmericanExpress",
+		};
+let invalid = {
+	       invalid: false,
+	       reason: "invalid length",
+	      };
+
+if(cardnumber[0] == 5 && cardnumber.length == 16) return Mastercard;
+else if(cardnumber[0] == 5 && cardnumber.length != 16) return invalid;
+if(cardnumber[0] == 4 && cardnumber.length == 16) return visacard;
+else if(cardnumber[0] == 4 && cardnumber.length != 16) return invalid;
+if(cardnumber[0] == 6 && cardnumber.length == 16) return Discover;
+else if(cardnumber[0] == 6 && cardnumber.length != 16) return invalid;
+if(cardnumber[0] == 3 && cardnumber.length == 15) return Discover;
+else if(cardnumber[0] == 3 && cardnumber.length != 15) return invalid;
+};
+
+firstnum = [1,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
+secondnum = [2,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
+thirdnum = [3,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
+fourthnum = [4,2,3,4,5,6,7,8,9,3,5,6,7,8,6];
+fifthnum = [5,2,3,4,5,6,7,w,9,3,5,6,7,8,6];
+
+
+
+
+let result = AtmValidation(fifthnum);
+
+console.log(result)
+
