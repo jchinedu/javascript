d52f9a612c6cb2a094f8e00a715f61088b4837c4
jchinedu <jchinedu080@gmail.com>
2025-06-07 00:05:53 +0100

    'git'

---
+++ Phase gate day 5/Test.java
@@ -0,0 +1,16 @@
+public class Test {
+public static void main(String[] args){
+int total = 0;
+int [] number = [1,2,3,4,5];
+for(int i = 0; i < number.length; i++){
+   total +=number[i];
+
+
+
+}
+System.out.print(total);
+
+
+
+}
+}
