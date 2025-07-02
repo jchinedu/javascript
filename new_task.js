1d598a6a1127168794b45c54c90a8d5012856dda
jchinedu <jchinedu080@gmail.com>
2025-07-02 15:53:45 +0100

    git

--- new_task.js
+++ new_task.js
@@ -1,13 +1,5 @@
-function Bug_Tracking_App(){
-    let list = [0,1,2,3,4,5,6,7,8,9,10]
-    let cleanse = [];
-    for(let index = 0; index < list.length; index++){
-        if(list[index]>0 && list[index] <=5){
-            cleanse.push(list[index])
-        }
-    }
-    return cleanse;
-    
+function Bug_Tracking_App(number){
+   return number.filter(number => number >=1 && number <=5)
 }
 
-console.log(Bug_Tracking_App())
+console.log(Bug_Tracking_App([1,2,3,4,5,6,7,8,9,0]))
